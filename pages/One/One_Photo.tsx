7f557a72dbc3234ec22249ee28495f6f8c955976
KangJekoo <peter6998@naver.com>
2023-11-05 15:27:37 +0900

    Feat: Finish Sketching 1 of Story Board

---
+++ pages/One/One_Photo.tsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+
+import {
+  Image,
+  Alert,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  Dimensions,
+  ScrollView,
+  View,
+} from "react-native";
+
+const One_Photo: React.FC = () => {
+  return(
+    <>
+    
+    
+    </>
+
+
+  )
+
+
+}
+
+
+export default One_Photo;
+
+
+
+
--- pages/One/One_Profile.tsx
+++ pages/One/One_Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import {
   Image,
   Alert,
@@ -6,14 +6,32 @@ import {
   SafeAreaView,
   TouchableOpacity,
   StyleSheet,
+  FlatList,
+  Dimensions,
+  ScrollView,
   View,
 } from "react-native";
-import { GuestBook, ProfilePhoto } from "../../assets/svg";
-import Icon from "react-native-vector-icons/Ionicons";
 
+//libs
+import Icon from "react-native-vector-icons/Ionicons";
+import { Modalize } from "react-native-modalize";
+
+//assets
+import { GuestBook, ProfilePhoto } from "../../assets/svg";
 import GlobalStyles from "../../assets/styles.tsx";
 
+//사진 랜더링 시 필요한 width 계산
+
+const numColumns = 3;
+const size = (Dimensions.get("window").width - 40) / numColumns;
+
+//FunctionComponents
 const One_Profile: React.FC = () => {
+  //방명록 모달
+  const GuestBookModal = useRef<Modalize>(null);
+  const openModal = () => GuestBookModal.current?.open();
+  const closeModal = () => GuestBookModal.current?.close();
+
   //전시회 업로더 프로필 조회
   //URL: users/{gallery_user_id}/profile (get)
   //응답:
@@ -46,10 +64,10 @@ const One_Profile: React.FC = () => {
   //   "exhibition_thumbnail" : ""
   //   }
   const currentExibitionDummy = {
-    exhibition_id: "",
-    exhibition_title: "",
-    exhibition_discription: "",
-    exhibition_thumbnail: "",
+    exhibition_id: "asdf",
+    exhibition_title: "Memory",
+    exhibition_discription: "2023-10 ~ 2023-11 Memories",
+    exhibition_thumbnail: require("../../assets/currentExibition.jpg"),
   };
 
   // 전시회 사진 받아오기
@@ -79,24 +97,64 @@ const One_Profile: React.FC = () => {
   const photoListDummy = {
     content: [
       {
-        photo_id: "asdf",
+        photo_id: "1",
         title: "Love",
-        photo: "",
+        photo: require("../../assets/photodummy1.jpg"),
         user_id: "",
         nickname: "",
         created_at: "",
       },
       {
-        photo_id: "",
+        photo_id: "2",
         title: "",
-        photo: "",
+        photo: require("../../assets/photodummy2.jpg"),
+        user_id: "",
+        nickname: "",
+        created_at: "",
+      },
+      {
+        photo_id: "3",
+        title: "",
+        photo: require("../../assets/photodummy3.jpg"),
+        user_id: "",
+        nickname: "",
+        created_at: "",
+      },
+      {
+        photo_id: "4",
+        title: "",
+        photo: require("../../assets/photodummy4.jpg"),
+        user_id: "",
+        nickname: "",
+        created_at: "",
+      },
+      {
+        photo_id: "5",
+        title: "",
+        photo: require("../../assets/photodummy5.jpg"),
         user_id: "",
         nickname: "",
         created_at: "",
       },
     ],
   };
+  const renderItem = ({ item }): React.JSX.Element => (
+    <TouchableOpacity
+      style={{
+        width: size,
+        height: size,
+        aspectRatio: 1,
+      }}
+    >
+      <Image
+        // source={{ uri: item.story }}
+        source={item.photo}
+        style={{ width: "100%", height: "100%" }}
+      />
+    </TouchableOpacity>
+  );
 
+  //구독 완료 전송 함수
   const submitSupport = () => {
     Alert.alert(
       "완료",
@@ -114,62 +172,100 @@ const One_Profile: React.FC = () => {
   };
 
   return (
-    <SafeAreaView style={GlobalStyles.container}>
-      {/* 1-1 맨 위 A's Gallery, Support Button 시작*/}
-      <View style={GlobalStyles.rowSpaceBetweenContainer}>
-        <Text
-          style={{
-            fontSize: 20,
-            fontWeight: "600",
-          }}
-        >
-          {userDataDummy.nickname}'s Gallery
-        </Text>
-        <TouchableOpacity
-          onPress={submitSupport}
-          style={GlobalStyles.backgroundBlackBox}
-        >
+    // (API연결시 랜더링 전 data 있는지 체크 후 랜더링 로직 추가)
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView style={GlobalStyles.container}>
+        {/* 1-1 맨 위 A's Gallery, Support Button 시작*/}
+        <View style={GlobalStyles.rowSpaceBetweenContainer}>
           <Text
             style={{
-              fontSize: 17,
-              fontWeight: "500",
-              color: "white",
+              fontSize: 20,
+              fontWeight: "600",
             }}
           >
-            Support
+            {userDataDummy.nickname}'s Gallery
           </Text>
+          <TouchableOpacity
+            onPress={submitSupport}
+            style={GlobalStyles.backgroundBlackBox}
+          >
+            <Text
+              style={{
+                fontSize: 17,
+                fontWeight: "500",
+                color: "white",
+              }}
+            >
+              Support
+            </Text>
+          </TouchableOpacity>
+        </View>
+        {/* 1-1 맨 위 A's Gallery, Support Button 끝*/}
+
+        {/* 1-1 프로필 사진, Biography, 방명록 아이콘 시작*/}
+        <View
+          style={{
+            ...GlobalStyles.rowSpaceBetweenContainer,
+            alignItems: "flex-start",
+          }}
+        >
+          <ProfilePhoto />
+          {/* <Image source={{ uri: userDataDummy.profile_img }} /> */}
+          <Text style={{ width: 130 }}>{userDataDummy.biography}</Text>
+
+          <TouchableOpacity onPress={openModal}>
+            <Icon name="reader-outline" size={27} color={"black"} />
+          </TouchableOpacity>
+        </View>
+        {/* 1-1 프로필 사진, Biography, 방명록 아이콘 끝*/}
+
+        {/* 현재 전시회 부분 시작 */}
+        <Text style={{ fontWeight: "500", marginBottom: 10 }}>
+          {userDataDummy.genres.map((e, i) => {
+            return e + "  ";
+          })}
+        </Text>
+        <Text style={{ fontWeight: "500", marginBottom: 10 }}>
+          {userDataDummy.equipments.map((e, i) => {
+            return e + "  ";
+          })}
+        </Text>
+        <Text style={{ fontSize: 17, fontWeight: "600", marginBottom: 10 }}>
+          Current Exibition
+        </Text>
+        <TouchableOpacity style={{ flexDirection: "row" }}>
+          <Image
+            source={currentExibitionDummy.exhibition_thumbnail}
+            style={{ width: 120, height: 120 }}
+          />
+          <View style={{ width: 120, marginLeft: 15 }}>
+            <Text style={{ fontWeight: "600" }}>
+              {currentExibitionDummy.exhibition_title}
+            </Text>
+            <Text>{currentExibitionDummy.exhibition_discription}</Text>
+          </View>
         </TouchableOpacity>
-      </View>
-      {/* 1-1 맨 위 A's Gallery, Support Button 끝*/}
+        {/* 현재 전시회 부분 끝 */}
 
-      {/* 1-1 프로필 사진, Biography, 방명록 아이콘 시작*/}
-      <View
-        style={{
-          ...GlobalStyles.rowSpaceBetweenContainer,
-          alignItems: "flex-start",
-        }}
-      >
-        <ProfilePhoto />
-        {/* <Image source={{ uri: userDataDummy.profile_img }} /> */}
-        <Text style={{ width: 130 }}>{userDataDummy.biography}</Text>
+        {/* 사진 나열 시작 */}
+        <Text style={{ fontSize: 17, fontWeight: "600", marginVertical: 10 }}>
+          Photographs
+        </Text>
+        <View>
+          <FlatList
+            data={photoListDummy.content}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.photo_id}
+            numColumns={3}
+            scrollEnabled={false}
+            // columnWrapperStyle={{ marginBottom: 5 }}
+            style={{ marginBottom: 50 }}
+            // onEndReached={loadMoreData}
+          />
+        </View>
+      </ScrollView>
 
-        <TouchableOpacity>
-          <Icon name="reader-outline" size={27} color={"black"} />
-        </TouchableOpacity>
-      </View>
-      {/* 1-1 프로필 사진, Biography, 방명록 아이콘 끝*/}
-
-      <Text style={{ fontWeight: "500", marginBottom: 10 }}>
-        {userDataDummy.genres.map((e, i) => {
-          return e + "  ";
-        })}
-      </Text>
-      <Text style={{ fontWeight: "500", marginBottom: 10 }}>
-        {userDataDummy.equipments.map((e, i) => {
-          return e + "  ";
-        })}
-      </Text>
-      <Text style={{ fontSize: 17, fontWeight: "600" }}>Current Exibition</Text>
+      <Modalize ref={GuestBookModal} adjustToContentHeight={true}></Modalize>
     </SafeAreaView>
   );
 };
